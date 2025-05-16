252b2f42f95ed1130d1b7bb0bc160e84db3e1f52
Parth Singh <dearparthsingh@gmail.com>
2025-05-16 08:57:09 +0530

    initial set-up

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import MarketingPage from './marketing-page/MarketingPage.tsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+    <MarketingPage/>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
